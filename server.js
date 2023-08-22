4367cf0ee556f0d630d3e47cccf69fa6510ac00b
Victor Marczyk <marczykv@gmail.com>
2023-08-21 22:15:30 -0400

    changed paths

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./src/routes/apiRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,19 +8,19 @@ const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/src/public'));
 
 // API Routes
 app.use('/api', apiRoutes);
 
 // Serve index.html for the root route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, './src/public/index.html'));
 });
 
 // Serve notes.html for the /notes route
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
+  res.sendFile(path.join(__dirname, './src/public/notes.html'));
 });
 
 // Start server
